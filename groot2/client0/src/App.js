ac3106dc6428dfb123e8720e6fb9da27184c9f5d
Ahmed <agedi98@gmail.com>
2018-10-06 05:28:51 -0400

    deleted client

---
+++ groot2/client0/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Team Groot!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
